d8c059593fe0104091ab72f9f9435ff149752d20
tomonori.matsukawa <maccho.chekera@gmail.com>
2022-09-12 22:44:53 +0900

    体重と体脂肪の小数点を1桁に修正

--- app/src/main/java/com/app/body_manage/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/app/body_manage/ui/setting/SettingScreen.kt
@@ -1,13 +1,19 @@
 package com.app.body_manage.ui.setting
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Switch
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.app.body_manage.common.BottomSheet
 import com.app.body_manage.common.BottomSheetData
 
@@ -24,7 +30,11 @@ fun SettingScreen(
     ) {
         when (state.value) {
             is SettingUiState.Settings -> {
-                Column(modifier = Modifier.padding(it)) {
+                Column(
+                    modifier = Modifier
+                        .padding(it)
+                        .padding(start = 12.dp, top = 12.dp)
+                ) {
                     val setting = state.value as SettingUiState.Settings
                     TextAndLabel(setting.alarm, notifyAction)
                 }
@@ -40,6 +50,19 @@ fun SettingScreen(
 
 @Composable
 private fun TextAndLabel(checked: Boolean, notifyAction: (Boolean) -> Unit) {
-    Text("設定画面")
-    Switch(checked = checked, onCheckedChange = notifyAction)
+    Row(
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "毎朝7時に通知",
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(start = 0.dp, end = 10.dp, top = 0.dp, bottom = 0.dp)
+        )
+        Switch(
+            checked = checked,
+            onCheckedChange = notifyAction,
+            modifier = Modifier.offset(y = 3.dp)
+        )
+    }
 }
