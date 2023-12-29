1571ff975d7aafd87265fc47d670c7a0de65c2a3
mtkw0127 <maccho.chekera@gmail.com>
2023-12-29 19:43:29 +0900

    Fix gradle to kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:8.2.0")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.8.0")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.21")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.10")
         classpath("de.mannodermaus.gradle.plugins:android-junit5:1.8.2.0")
         classpath("com.google.gms:google-services:4.4.0")
         classpath("com.google.firebase:firebase-crashlytics-gradle:2.9.9")
