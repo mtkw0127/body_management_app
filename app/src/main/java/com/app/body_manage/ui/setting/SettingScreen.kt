126de0d7c21c6ffacbb68869afad1c713d36eebe
mtkw0127 <maccho.chekera@gmail.com>
2024-01-02 16:19:52 +0900

    Fix detekt

--- app/src/main/java/com/app/body_manage/ui/setting/SettingScreen.kt
+++ app/src/main/java/com/app/body_manage/ui/setting/SettingScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.app.body_manage.R
