7b6dc7d98be4ade5c0dbcc5aea96f539ebb4993a
mtkw0127 <maccho.chekera@gmail.com>
2024-05-02 00:01:24 +0900

    Bump up

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.app.calendar"
         minSdk = 26
         targetSdk = 34
-        versionCode = 32
-        versionName = "1.31.0"
+        versionCode = 33
+        versionName = "1.31.1"
         applicationId = "com.app.body_manage"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
